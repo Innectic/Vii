7fd118a73bcca8beb2b3c34fa94dbd76e99d6b89
Innectic <innectic@gmail.com>
2017-10-07 02:38:00 -0700

    Cleanup, and fixes for Workspaces.

--- src/workspace.cpp
+++ src/workspace.cpp
@@ -7,12 +7,22 @@ const std::vector<std::string> splitString(const std::string& splitting, const s
 	std::size_t current, previous = 0;
 	current = splitting.find_first_of(delim);
 	while (current != std::string::npos) {
-		matches.emplace_back(splitting.substr(previous, current - previous));
+		std::string matched = splitting.substr(previous, current - previous);
+		matches.emplace_back(matched);
 		previous = current + 1;
 		current = splitting.find_first_of(delim, previous);
 	}
 	matches.emplace_back(splitting.substr(previous, current - previous));
-	return matches;
+
+	// TODO: Why can't this be in the main loop?
+	// It seems to make the entire function hang and never exit, for some reason.
+	std::vector<std::string> clean;
+	for (auto dirty : matches) {
+		if (dirty == " " || dirty == "") continue;
+		clean.emplace_back(dirty);
+	}
+
+	return clean;
 }
 
 void WorkSpace::setDefaults() {
@@ -22,6 +32,7 @@ void WorkSpace::setDefaults() {
 	this->blockCommentEnd = "*/";
 
 	this->commentPrefix = "//";
+	this->optimization = OptimizationLevel::OP_NONE;
 }
 
 void WorkSpace::loadConfiguration() {
@@ -33,15 +44,8 @@ void WorkSpace::loadConfiguration() {
 		// Remove all spaces from the string. We don't REALLY need them.
 		std::string::iterator end_pos = std::remove(line.begin(), line.end(), ' ');
 		line.erase(end_pos, line.end());
-
-		auto dirtyMatches = splitString(line, ":=");
-		std::vector<std::string> matches;
-		// TODO: /shrug why is this a thing
-		for (auto dirty : dirtyMatches) {
-			if (dirty == "" || dirty == " ") continue;
-			matches.emplace_back(dirty);
-		}
-
+		
+		auto matches = splitString(line, ":=");		
 		if (matches.size() < 2) {
 			std::cout << "Configuration option " << matches[0] << " has no value!" << std::endl;
 			continue;
@@ -53,12 +57,14 @@ void WorkSpace::loadConfiguration() {
 		
 		if (lhs == "optimization") {
 			if (rhs == "none") this->optimization = OptimizationLevel::OP_NONE;
-			if (rhs == "low") this->optimization = OptimizationLevel::OP_LOW;
-			if (rhs == "medium") this->optimization = OptimizationLevel::OP_MEDIUM;
-			if (rhs == "max") this->optimization = OptimizationLevel::OP_MAX;
-			if (rhs == "size") this->optimization = OptimizationLevel::OP_SIZE;
-			if (rhs == "speed") this->optimization = OptimizationLevel::OP_SPEED;
-			std::cout << "Invalid value for optimization level: " << rhs << std::endl;
+			else if (rhs == "low") this->optimization = OptimizationLevel::OP_LOW;
+			else if (rhs == "medium") this->optimization = OptimizationLevel::OP_MEDIUM;
+			else if (rhs == "max") this->optimization = OptimizationLevel::OP_MAX;
+			else if (rhs == "size") this->optimization = OptimizationLevel::OP_SIZE;
+			else if (rhs == "speed") this->optimization = OptimizationLevel::OP_SPEED;
+			else std::cout << "Invalid value for optimization level: " << rhs << std::endl;
 		}
 	}
+
+	std::cout << this->optimization << std::endl;
 }
