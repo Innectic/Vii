5f3e004d7698db40ba98bac998a3efa800c2ca7b
Innectic <innectic@gmail.com>
2017-11-08 09:12:39 -0800

    Fix drive problems for windows builds

--- compile_windows.bat
+++ compile_windows.bat
@@ -3,7 +3,6 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 echo Compiling %2
-E:
-cd %1
+cd /D %1
 
 cl %1\%2 /nologo /std:c++latest /EHsc /Gr
