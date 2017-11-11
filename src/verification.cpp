48c3c3f1cc67e308e55e584d860d091c957d0ae6
Innectic <innectic@gmail.com>
2017-11-11 08:55:51 -0800

    Remove debug code

--- src/verification.cpp
+++ src/verification.cpp
@@ -5,10 +5,6 @@ const bool Verification::validate(const std::vector<AST_Type*>& contained) {
     for (auto component : contained) {
         if (is_type<AST_Function*>(component)) {
             auto function = static_cast<AST_Function*>(component);
-            std::cout << function->name << std::endl;
-            std::string argument_string;
-            for (auto arg : function->arguments) argument_string += token_map[arg.type] + " ";
-            std::cout << "Func args: " << argument_string << std::endl;
             if (function->contained.size() > 0) this->validate(function->contained);
             // Make sure something like this doesn't already exist
             auto resolved_function_type = new AST_Resolved_Function(function->name, function->line, function->column, function->arguments, function->scope);
@@ -31,9 +27,6 @@ const bool Verification::validate(const std::vector<AST_Type*>& contained) {
                 this->workspace.report_error({ "Function with " + argument_string + "by name " + function_call->name + " doesn't exist.", "[file]", function_call->line, function_call->column });
                 continue;
             }
-            std::string argument_string;
-            for (auto arg : function_call->arguments) argument_string += token_map[arg.type] + " ";
-            std::cout << "Woah, " << function_call->name << " with args " + argument_string + "was actually resolved" << std::endl;
         }
     }
     return true;
