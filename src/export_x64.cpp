36a8977d5bdb87dd2ecfc78e21237fa77176111d
Innectic <innectic@gmail.com>
2018-01-06 22:49:30 -0800

    Cleanup exporter more

--- src/export_x64.cpp
+++ src/export_x64.cpp
@@ -22,6 +22,8 @@ const std::string convert_type(const TokenType& type) {
 }
 
 const std::string build_argument_string(AST_FunctionCall* function, bool include_type) {
+	assert(function);
+
 	std::string argument_string = "";
 
 	for (auto arg = function->arguments.begin(); arg < function->arguments.end(); arg++) {
@@ -51,6 +53,8 @@ const void add_import(const std::string& lib, std::ofstream& stream) {
     stream << line;
 }
 
+Export_x64::Export_x64(bool allow_native) : allow_native(allow_native) {}
+
 const std::string Export_x64::name() const {
     return "x64";
 }
@@ -58,29 +62,21 @@ const std::string Export_x64::name() const {
 const void handle_math_segment(AST_Math* math, std::ofstream& stream) {
 	assert(math);
 
-    for (auto i = 0u; i < math->operations.size(); i++) {
-        auto op = math->operations[i];
-        auto last = i == math->operations.size() - 1;
-
-        if (op.chain) stream << token_map[op.operation] << op.second_value;
-        else stream << op.first_value << token_map[op.operation] << op.second_value;
-    }
-}
-
-const void Export_x64::begin(const AST_SourceFile& source_file, std::ofstream& stream, bool should_import) {
-	this->begin(source_file.contained, stream, should_import, source_file.mainFunction);
-}
-
-const void Export_x64::begin(const std::vector<AST_Type*> contained, std::ofstream& stream, bool should_import, AST_Function* main_function) {
-	if (should_import) {
-		add_import("<string>", stream);
-		add_import("<iostream>", stream);
-		if (!allow_native) add_import("\"io.cpp\"", stream);
+	for (auto it = math->operations.begin(); it < math->operations.end(); it++) {
+		if (it->chain) stream << token_map[it->operation] << it->second_value;
+		else stream << it->first_value << token_map[it->operation] << it->second_value;
 	}
+}
 
+const void Export_x64::begin(const AST_SourceFile& source_file, std::ofstream& stream) {
+	this->begin(source_file.contained, stream, source_file.mainFunction);
+}
+
+const void Export_x64::begin(const std::vector<AST_Type*> contained, std::ofstream& stream, AST_Function* main_function) {
 	for (auto& potential : contained) {
 		if (is_type<AST_Declaration*>(potential)) {
 			auto decl = static_cast<AST_Declaration*>(potential);
+			assert(decl);
 
 			std::string type = decl->redecl ? "" : convert_type(decl->type);
 			stream << type + " " + decl->name + " = ";
@@ -111,6 +107,8 @@ const void Export_x64::begin(const std::vector<AST_Type*> contained, std::ofstre
 			stream << "}\n";
 		} if (is_type<AST_Builtin*>(potential)) {
 			auto builtin = static_cast<AST_Builtin*>(potential);
+			assert(builtin);
+
 			std::string ready_line = allow_native ? native_map[builtin->type] : internal_map[builtin->type];
 			ready_line = Util::replace(ready_line, "<CUSTOM>", build_argument_string(builtin, false));
 			stream << ready_line + "\n";
@@ -119,19 +117,21 @@ const void Export_x64::begin(const std::vector<AST_Type*> contained, std::ofstre
 			// Are we defining a function?
 			if (is_type<AST_Function*>(potential)) {
 				auto function = static_cast<AST_Function*>(potential);
-				// Check if this is the main function
-				auto function_name = function->name;
-				if (main_function && main_function == function) function_name = "real_main";
+				assert(function);
 
+				auto function_name = main_function && main_function == function ? "real_main" : function->name;
+				
 				auto return_type = convert_type(function->return_type);
 				stream << return_type + " " + function_name + "(" + build_argument_string(function, true) + ") {\n";
 				
-				this->begin(function->contained, stream, false, nullptr);
+				this->begin(function->contained, stream, nullptr);
 				stream << "}\n";
 				continue;
 			}
 			// Nope, so we're calling one.
 			auto call = static_cast<AST_FunctionCall*>(potential);
+			assert(call);
+
 			if (call->native) {
 				if (!this->allow_native) {
 					std::cout << "Cannot define or call a native method unless this is the std lib." << std::endl;
@@ -145,20 +145,27 @@ const void Export_x64::begin(const std::vector<AST_Type*> contained, std::ofstre
 		}
 		else if (is_type<AST_If_Block*>(potential)) {
 			auto if_block = static_cast<AST_If_Block*>(potential);
+			assert(if_block);
 
 			// Export the main if
 			stream << "if (" << if_block->first_block->conditional << ") {\n";
-			this->begin(*if_block->first_block->contained, stream, false);
+			this->begin(*if_block->first_block->contained, stream);
 			stream << "}\n";
 			// TODO: Else if / else
 		}
 	}
-	if (!allow_native && should_import) stream << FILE_TEMPLATE;
+	if (!allow_native) stream << FILE_TEMPLATE;
 }
 
 const void Export_x64::go(const std::string& destination_file, const AST_SourceFile& source_file) {
     std::ofstream stream;
     stream.open(destination_file);
 
-    this->begin(source_file, stream, true);
+	// Import all the things
+	add_import("<string>", stream);
+	add_import("<iostream>", stream);
+	if (!allow_native) add_import("\"io.cpp\"", stream);
+
+	// Export the AST tree to C++ code.
+    this->begin(source_file, stream);
 }
--- src/export_x64.h
+++ src/export_x64.h
@@ -9,13 +9,11 @@ class Export_x64 : public Exporter {
 private:
     bool allow_native;
 public:
-    inline Export_x64(bool allow_native) : allow_native(allow_native) {
-
-    }
+	Export_x64(bool allow_native);
 
     std::ofstream stream;
     const std::string name() const override;
-	const void begin(const AST_SourceFile& source_file, std::ofstream& stream, bool should_import) override;
-	const void begin(const std::vector<AST_Type*> contained, std::ofstream& stream, bool should_import, AST_Function* main_function) override;
+	const void begin(const AST_SourceFile& source_file, std::ofstream& stream) override;
+	const void begin(const std::vector<AST_Type*> contained, std::ofstream& stream, AST_Function* main_function) override;
     const void go(const std::string& destination_file, const AST_SourceFile& source_file) override;
 };
